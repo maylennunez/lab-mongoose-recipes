005890891f8eb2ca342736a70d43639e57b3adfe
Maylen Nunez <maylennunez@hotmail.com>
2020-03-15 16:59:28 -0400

    changes

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+  // Iteration 2 - Create a recipe
+
+  //  Recipe.create({
+  //   title: 'Strudel',
+  //   cuisine: "Italian"
+  //  })
+  //  .then((res) => console.log(res))
+
+  //  Iteration 3 - Insert multiple recipes
+   
+  // Recipe.insertMany(data)
+  // .then((res)=> console.log(res))
+  // .catch((err) => { console.log('An error happened:', err) });
+
+  // Iteration 4 - Update recipe
+
+  Recipe.updateOne({title:'Rigatoni alla Genovese'}, {duration:100})
+.then((res) => console.log(res))
+.catch((err) => { console.log('An error happened:',err)})
+
+// Iteration 5 - Remove a recipe
+
+Recipe.deleteOne({title:'Carrot Cake'})
+.then(res => console.log('Carrot Cake is no longer available'))
+.catch((err)=> {console.log('An error happened:',err)})
